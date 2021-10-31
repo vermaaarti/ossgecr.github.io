57d9ca1ef4d198be30daff18ea02a76132f7f960
niitish <nitish.headfirst@outlook.com>
2021-10-31 19:53:19 +0530

    Router->BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Header from "./Header";
-import { Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <Router>
+  <BrowserRouter>
     <Header />
-  </Router>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
