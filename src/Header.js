857b82e264c6e6b02599a3fd684d57d7cae408a9
niitish <nitish.headfirst@outlook.com>
2021-10-31 19:43:53 +0530

    back to column styled nav-links

--- src/Header.js
+++ src/Header.js
@@ -21,26 +21,26 @@ function Header() {
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="m-auto">
-                <div className="d-flex justify-content-evenly">
-                  <Nav.Link>
-                    <Link to="/events">Events</Link>
-                  </Nav.Link>
-                  <Nav.Link>
-                    <Link to="/members">Members</Link>
-                  </Nav.Link>
-                  <Nav.Link>
-                    <Link to="/about">About</Link>
-                  </Nav.Link>
-                  <Nav.Link>
-                    <Link to="/contact">Contact</Link>
-                  </Nav.Link>
-                </div>
+                <Nav.Link>
+                  <Link to="/events">Events</Link>
+                </Nav.Link>
+                <Nav.Link>
+                  <Link to="/members">Members</Link>
+                </Nav.Link>
+                <Nav.Link>
+                  <Link to="/about">About</Link>
+                </Nav.Link>
+                <Nav.Link>
+                  <Link to="/contact">Contact</Link>
+                </Nav.Link>
               </Nav>
               <div className="d-flex justify-content-evenly">
-                <Nav>
-                  <div className={styles.card}>
-                    <Link to="/login">Login</Link>
-                  </div>
+                <Nav className="m-auto">
+                  <Nav.Link>
+                    <div className={styles.card}>
+                      <Link to="/login">Login</Link>
+                    </div>
+                  </Nav.Link>
                 </Nav>
               </div>
             </Navbar.Collapse>
