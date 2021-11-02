16c0631f80fcd90ca0be74554379d9b70b6112b0
niitish <nitish.headfirst@outlook.com>
2021-11-02 21:57:50 +0530

    added Comingsoon component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Header from "./Header";
 import { BrowserRouter } from "react-router-dom";
+import Comingsoon from "./Comingsoon";
 
 ReactDOM.render(
   <BrowserRouter>
     <Header />
+    <Comingsoon />
   </BrowserRouter>,
   document.getElementById("root")
 );
