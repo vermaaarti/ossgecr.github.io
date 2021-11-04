2e2024baeeff1693819a56d710006e5fc36587dd
niitish <nitish.headfirst@outlook.com>
2021-11-04 10:35:51 +0530

    slight modfications

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import Events from "./Events";
 import Header from "./Header";
+import Events from "./Events";
 import Members from "./Members";
+import About from "./About";
+import Contact from "./Contact";
 import Comingsoon from "./Comingsoon";
 import { Route, Switch } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
       <Switch>
-        <Route path="/" exact>
-          <Comingsoon />
-        </Route>
-        <Route path="/events" exact>
-          <Events />
-        </Route>
-        <Route path="/members" exact>
-          <Members />
-        </Route>
-        <Route path="/about" exact></Route>
-        <Route path="/contact" exact></Route>
+        <Route exact path="/" component={Comingsoon} />
+        <Route exact path="/events" component={Events} />
+        <Route exact path="/members" component={Members} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
       </Switch>
-    </div>
+    </BrowserRouter>
   );
 }
 
