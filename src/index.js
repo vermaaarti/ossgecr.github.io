6358823ae96d2584486014fd14ffb1ee9d3fa041
niitish <nitish.headfirst@outlook.com>
2021-11-04 10:37:32 +0530

    remove BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,11 +2,5 @@ import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
