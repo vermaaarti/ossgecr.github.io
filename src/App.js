0e3ce11ab3c6699a28bdc4c7e7795b6afe7e51cc
niitish <nitish.headfirst@outlook.com>
2021-11-04 08:50:37 +0530

    move app logic from index to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import Events from "./Events";
+import Header from "./Header";
+import Members from "./Members";
+import Comingsoon from "./Comingsoon";
+import { Route, Switch } from "react-router-dom";
+
 function App() {
   return (
-    <div>hello</div>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <Comingsoon />
+        </Route>
+        <Route path="/events" exact>
+          <Events />
+        </Route>
+        <Route path="/members" exact>
+          <Members />
+        </Route>
+        <Route path="/about" exact></Route>
+        <Route path="/contact" exact></Route>
+      </Switch>
+    </div>
   );
 }
 
