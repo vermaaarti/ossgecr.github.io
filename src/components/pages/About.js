5cfa565d1e71fc2a39c6afb3c387f07bf7889235
niitish <nitish.headfirst@outlook.com>
2021-11-12 21:36:02 +0530

    Change placeholder text

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,7 +1,11 @@
 import "./style/centerContent.css";
 
 function About() {
-  return <div className="centcnt">Nothing here yet.</div>;
+  return (
+    <div className="centcnt">
+      About us content here. Goals, mission, etc. will be shown on this page.
+    </div>
+  );
 }
 
 export default About;
