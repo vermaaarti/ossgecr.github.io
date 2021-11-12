d3b0ddf36d9ea3462efff844a365d285d52239e5
niitish <nitish.headfirst@outlook.com>
2021-11-12 21:35:58 +0530

    Change placeholder text

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,11 @@
 import "./style/centerContent.css";
 
 function Contact() {
-  return <div className="centcnt">Nothing here yet.</div>;
+  return (
+    <div className="centcnt">
+      Contact information like mail, phone, classroom number, etc. here.
+    </div>
+  );
 }
 
 export default Contact;
