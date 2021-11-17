fc49507c5bfa564ac527068ddda3d2da3d978abb
niitish <nitish.headfirst@outlook.com>
2021-11-17 19:00:50 +0530

    remove login form

--- src/components/pages/Loginpage.js
+++ src/components/pages/Loginpage.js
@@ -1,27 +1,5 @@
-import "./style/centerContent.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Form, Button } from "react-bootstrap";
-
 const Loginpage = () => {
-  return (
-    <div className="centcnt">
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="Username" />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Loginpage;
