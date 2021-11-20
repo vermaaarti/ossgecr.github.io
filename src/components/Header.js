2139e7d9f18b9875d6970a877547978d5d6b9ee7
niitish <nitish.headfirst@outlook.com>
2021-11-20 19:35:42 +0530

    add routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
+import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 
 const Header = () => {
   return (
     <div className={styles.header}>
       <div className={styles.navleft}>
-        <button className="btn btn-sm btn-ghost">Events</button>
-        <button className="btn btn-sm btn-ghost">Projects</button>
-        <button className="btn btn-sm btn-ghost">Members</button>
+        <button className="btn btn-sm btn-ghost">
+          <Link to="/events">Events</Link>
+        </button>
+        <button className="btn btn-sm btn-ghost">
+          <Link to="/projects">Projects</Link>
+        </button>
+        <button className="btn btn-sm btn-ghost">
+          <Link to="/members">Members</Link>
+        </button>
       </div>
 
       <div className={styles.navleftmob}>
@@ -31,45 +38,76 @@ const Header = () => {
             className="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-35"
           >
             <li>
-              <a>Events</a>
+              <a>
+                <Link to="/events">Events</Link>
+              </a>
             </li>
             <li>
-              <a>Projects</a>
+              <a>
+                <Link to="/projects">Projects</Link>
+              </a>
             </li>
             <li>
-              <a>Members</a>
+              <a>
+                <Link to="/members">Members</Link>
+              </a>
+            </li>
+            <li>
+              <a>
+                <Link to="/">Blog</Link>
+              </a>
             </li>
           </ul>
         </div>
       </div>
 
       <div className={styles.logo}>
-        <button className="btn btn-md btn-ghost">GOSS</button>
+        <button className="btn btn-md btn-ghost">
+          <Link to="/">OSS</Link>
+        </button>
       </div>
 
       <div className={styles.navrightmob}>
         <button className={`btn btn-sm btn-ghost btn-circle ${styles.github}`}>
-          <svg
-            height="32"
-            aria-hidden="true"
-            viewBox="0 0 16 16"
-            version="1.1"
-            width="32"
-            data-view-component="true"
-            className="octicon octicon-mark-github"
+          <Link
+            to={{
+              pathname: "https://github.com/ossgecr",
+            }}
+            target="_blank"
           >
-            <path
-              fill-rule="evenodd"
-              d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
-            ></path>
-          </svg>
+            <svg
+              height="32"
+              aria-hidden="true"
+              viewBox="0 0 16 16"
+              version="1.1"
+              width="32"
+              data-view-component="true"
+              className="octicon octicon-mark-github"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+              ></path>
+            </svg>
+          </Link>
         </button>
       </div>
 
       <div className={styles.navright}>
-        <button className="btn btn-sm btn-outline">GitHub</button>
+        <button className="btn btn-sm btn-outline">
+          <Link
+            to={{
+              pathname: "https://github.com/ossgecr",
+            }}
+            target="_blank"
+          >
+            GitHub
+          </Link>
+        </button>
         <button className="btn btn-sm btn-outline">Blog</button>
-        <button className="btn btn-sm btn-outline btn-primary">Login</button>
+        <button className="btn btn-sm btn-outline btn-primary">
+          <Link to="/login">Login</Link>
+        </button>
       </div>
     </div>
   );
