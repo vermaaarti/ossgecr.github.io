f1bbedbd555b87de383db1f496bfb769dd76b1a1
niitish <nitish.headfirst@outlook.com>
2021-11-21 13:10:49 +0530

    about + members

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,47 @@
-const About = () => {
+import styles from "./style/About.module.css";
+import ProfileCard from "../ui/ProfileCard";
+
+const Members = () => {
   return (
     <div>
-      About us content here. Goals, mission, etc. will be shown on this page.
+      <div className={styles.title}>
+        <h2>Our team</h2>
+      </div>
+      <div className={styles.container}>
+        <ProfileCard
+          img="https://picsum.photos/id/1005/400/250"
+          name="Test Name"
+          role="Lead Developer"
+          about="The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value."
+          github="https://github.com/firefox"
+        />
+        <ProfileCard
+          img="https://picsum.photos/id/1005/400/250"
+          name="Test Name"
+          role="Lead Developer"
+          about="The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value."
+        />
+        <ProfileCard
+          img="https://picsum.photos/id/1005/400/250"
+          name="Test Name"
+          role="Lead Developer"
+          about="The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value."
+        />
+        <ProfileCard
+          img="https://picsum.photos/id/1005/400/250"
+          name="Test Name"
+          role="Lead Developer"
+          about="The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value."
+        />
+        <ProfileCard
+          img="https://picsum.photos/id/1005/400/250"
+          name="Test Name"
+          role="Lead Developer"
+          about="The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value."
+        />
+      </div>
     </div>
   );
 };
 
-export default About;
+export default Members;
