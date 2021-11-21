82b043914a982137d198f65c22f726ec891f20cb
niitish <nitish.headfirst@outlook.com>
2021-11-21 13:12:18 +0530

    remove unused

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import Header from "./components/Header";
 import Events from "./components/pages/Events";
-import Members from "./components/pages/Members";
 import About from "./components/pages/About";
-import Contact from "./components/pages/Contact";
 import Comingsoon from "./components/pages/Comingsoon";
 import Loginpage from "./components/pages/Loginpage";
 import Errorpage from "./components/pages/404";
@@ -17,7 +15,7 @@ const App = () => {
         <Route exact path="/" component={Comingsoon} />
         <Route exact path="/events" component={Events} />
         <Route exact path="/projects" component={Projects} />
-        <Route exact path="/members" component={Members} />
+        <Route exact path="/about" component={About} />
         <Route exact path="/login" component={Loginpage} />
         <Route path="" component={Errorpage} />
       </Switch>
